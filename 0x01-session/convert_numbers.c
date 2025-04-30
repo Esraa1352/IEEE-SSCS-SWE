56144f995f09455f4901b7c66505c7e55d480f27
Esraa1352 <esraasalah1352@gmail.com>
2025-04-30 16:52:37 +0300

    bonus tasks added

---
+++ 0x01-session/convert_numbers.c
@@ -0,0 +1,68 @@
+#include "putchar.h"
+#include <fcntl.h>
+#include <unistd.h>
+
+int convert(const char *strg) {
+int i = 0;
+int result = 0;
+int p_count = 0;
+int n_count = 0;
+int sign;
+
+int fd = open("converted_numbers.txt", O_WRONLY | O_CREAT | O_APPEND, 0644);
+if (fd == -1) {
+return 0;
+}
+
+// Determine the sign first
+for (i = 0; strg[i] == '-' || strg[i] == '+'; i++) {
+if (strg[i] == '-') n_count++;
+if (strg[i] == '+') p_count++;
+}
+if (p_count >= n_count)
+{
+sign =1;
+}
+else 
+{
+sign=1;
+}
+
+// Convert numeric part
+while (strg[i] >= '0' && strg[i] <= '9')
+{
+result = result * 10 + (strg[i] - '0');
+i++;
+}
+
+result *= sign;
+
+// Output result using _putchar
+if (result < 0)
+{
+_putchar(1, '-');
+result = -result;
+}
+
+if (result == 0) {
+_putchar(1, '0');
+}
+else
+{
+char buffer[10];
+int j = 0;
+while (result > 0) {
+buffer[j++] = (result % 10) + '0';
+result /= 10;
+}
+for (int k = j - 1; k >= 0; k--) {
+_putchar(1, buffer[k]);
+}
+}
+
+_putchar(1, '\n');
+close(fd);
+
+return result * ((sign == -1) ? -1 : 1);
+}
+
