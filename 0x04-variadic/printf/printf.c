f7a683678b78ee66a28960a996f73ff958b3db37
Esraa1352 <esraasalah1352@gmail.com>
2025-07-07 20:03:17 +0300

    printf added

--- 0x04-variadic/printf/printf.c
+++ 0x04-variadic/printf/printf.c
@@ -61,6 +61,7 @@ int _printf(const char *format, ...)
                     }
                 }
             }
+
             else if (format[i] == 's')
             {
                 char *string = va_arg(args, char *);
@@ -111,6 +112,108 @@ int _printf(const char *format, ...)
                     }
                 }
             }
+                else if (format[i] == 'o')
+                {
+                    int num = va_arg(args, int);
+                    
+                    if (num == 0)
+                    {
+                        _putchar('0');
+                        count++;
+                    }
+                    else
+                    {
+                        char octal[8]; 
+                        int j = 0;
+    
+                        if (num < 0)
+                        {
+                            _putchar('-');
+                            num = -num;
+                            count++;
+                        }
+    
+                        while (num > 0)
+                        {
+                            octal[j++] = (num % 8) + '0';
+                            num = num / 8;
+                        }
+    
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            _putchar(octal[k]);
+                            count++;
+                        }
+                    }
+                }
+                else if (format[i] == 'h')
+                {
+                    int num = va_arg(args, int);
+                
+                    if (num == 0)
+                    {
+                        _putchar('0');
+                        count++;
+                    }
+                    else
+                    {
+                        char hexa[10];
+                        int j = 0;
+                
+                        if (num < 0)
+                        {
+                            _putchar('-');
+                            num = -num;
+                            count++;
+                        }
+                
+                        while (num > 0)
+                        {
+                            int digit = num % 16;
+                            if (digit < 10)
+                                hexa[j++] = digit + '0';
+                            else
+                                hexa[j++] = digit - 10 + 'a'; 
+                            num = num / 16;
+                        }
+                
+                        for (int k = j - 1; k >= 0; k--)
+                        {
+                            _putchar(hexa[k]);
+                            count++;
+                        }
+                    }
+                }
+                
+                else if (format[i] == 'u')
+                {
+                    unsigned int num = va_arg(args, unsigned int);
+
+                        if (num == 0)
+                        {
+                            _putchar('0');
+                            count++;
+                        }
+                        else
+                        {
+                        char arr[10];  
+                        int m = 0;
+
+                        while (num > 0)
+                        {
+                            arr[m++] = (num % 10) + '0'; 
+                            num = num / 10;
+                        }
+
+                        for (int j = m - 1; j >= 0; j--)
+                        {
+                            _putchar(arr[j]);
+                            count++;
+                        }
+                    }
+                }
+
+
             else
             {
                 _putchar('%');
@@ -139,7 +242,11 @@ int main(void)
     count = _printf("Percent sign test: %%\n");
     count = _printf("NULL string test: %s\n", (char *)NULL);
     count = _printf("Unknown specifier test: %q\n");
-    count = _printf("Unknown specifier test: %b\n" , 123);
+    count = _printf("Binary conversion test: %b\n" , 123);
+    count = _printf("Octal conversion test: %o\n" , 123);
+    count = _printf("unsigned integer test: %u\n" , -1);
+    count = _printf("hexadecimal conversion test: %h\n" , 123);
+
     _printf("number of printed characters test: %d\n", count);
 
     return 0;
